979c13bc86557a1b72c13db58812f3c1ba1026ee
Da-YTH <danielyitayeh@example.com>
2022-09-08 11:30:42 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+/**
+ *
+ * *main - print the string in the put function
+ *
+ * *
+ *
+ * *Description: using the main function 
+ *
+ * *this program prints "Programing is like building a multilingual puzzle
+ *
+ * *Return: 0
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programing is like building a multilingual puzzle");
+
+}
+
+
