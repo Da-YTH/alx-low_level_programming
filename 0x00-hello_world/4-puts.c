d42613a39736c9fe7ebc36a03ba6c46806d4264b
Da-YTH <danielyitayeh@example.com>
2022-09-08 11:49:13 -0700

    puts.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,19 @@
+#include <stdio.h>
+
 /**
+ * main - print the string in the put function
+ * Description: using the main function 
+ * this program prints "Programming is like building a multilingual puzzle
  *
- * *main - print the string in the put function
+ * Return: 0
  *
- * *
- *
- * *Description: using the main function 
- *
- * *this program prints "Programing is like building a multilingual puzzle
- *
- * *Return: 0
- *
- * */
+ * /
 
 int main(void)
 
 {
 
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 }
 
